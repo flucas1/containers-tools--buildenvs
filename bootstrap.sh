c98f59af3c35f1d3e65c2bdd043634e5526f800c
Fernando Lucas <info@fernandolucas.info>
2024-10-28 15:33:48 +0100

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,8 @@ sysbench memory run
 
 http_proxy=${APTCACHER} DEBIAN_FRONTEND=noninteractive apt-get install uidmap containers-storage fuse-overlayfs fuse3 iptables netavark aardvark-dns buildah podman skopeo --no-install-recommends -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold"
 cp -f /usr/share/containers/storage.conf /etc/containers/storage.conf
-sed -i -e 's|^#mount_program|mount_program|g' /etc/containers/storage.conf
-sed -i -e 's|^driver = ""|driver = "overlay"|g' /etc/containers/storage.conf
+#sed -i -e 's|^#mount_program|mount_program|g' /etc/containers/storage.conf
+#sed -i -e 's|^driver = ""|driver = "overlay"|g' /etc/containers/storage.conf
 cat /etc/containers/storage.conf
 ls /dev/fuse || true
 buildah -v
