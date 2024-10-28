89a1ba8402f9ed0bd6c14d500f449f4734c08f59
Fernando Lucas <info@fernandolucas.info>
2024-10-28 14:49:52 +0100

    cat /etc/containers/storage.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,7 @@ http_proxy=${APTCACHER} DEBIAN_FRONTEND=noninteractive apt-get install uidmap co
 cp -f /usr/share/containers/storage.conf /etc/containers/storage.conf
 sed -i -e 's|^#mount_program|mount_program|g' /etc/containers/storage.conf
 sed -i -e 's|^driver = ""|driver = "overlay2"|g' /etc/containers/storage.conf
+cat /etc/containers/storage.conf
 buildah -v
 podman -v
 skopeo -v
