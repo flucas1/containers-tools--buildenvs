833f5c5399717aa5b8208a6208f09da097d38e09
Fernando Lucas <info@fernandolucas.info>
2024-10-28 15:08:06 +0100

    --device /dev/fuse

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,15 @@ http_proxy=${APTCACHER} DEBIAN_FRONTEND=noninteractive apt-get update
 http_proxy=${APTCACHER} DEBIAN_FRONTEND=noninteractive apt-get install tzdata ca-certificates jq sysbench git rsync --no-install-recommends -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold"
 sysbench cpu run
 sysbench memory run
+
 http_proxy=${APTCACHER} DEBIAN_FRONTEND=noninteractive apt-get install uidmap containers-storage fuse-overlayfs fuse3 iptables netavark aardvark-dns buildah podman skopeo --no-install-recommends -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold"
 cp -f /usr/share/containers/storage.conf /etc/containers/storage.conf
 sed -i -e 's|^#mount_program|mount_program|g' /etc/containers/storage.conf
-sed -i -e 's|^driver = ""|driver = "overlay2"|g' /etc/containers/storage.conf
-sed -i -e 's|^driver = "overlay"|driver = "overlay2"|g' /etc/containers/storage.conf
+sed -i -e 's|^driver = ""|driver = "overlay"|g' /etc/containers/storage.conf
 cat /etc/containers/storage.conf
+ls /dev/fuse
 buildah -v
 podman -v
 skopeo -v
+
 http_proxy=${APTCACHER} DEBIAN_FRONTEND=noninteractive apt-get install git --no-install-recommends -y -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold"
